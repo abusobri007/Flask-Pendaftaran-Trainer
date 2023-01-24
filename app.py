df8f1fb94a8267d338c641b7f78c18ac84b4b3cf
briisco <abusobri007@gmail.com>
2023-01-24 22:58:55 +0800

    feat: membuat route list pendaftaran menggunkan flask -m feat: membuat fitur di html menggunkan html

--- app.py
+++ app.py
@@ -8,16 +8,32 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///perserta.db'
-app.config['UPLOAD_FOLDER'] = os.path.abspath(os.path.dirname(__file__)) + "\static\media/"
-ALLOWED_EXTENSIONS = { 'pdf', 'png', 'jpg', 'jpeg'}
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+#Model
+class Peserta(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  nama = db.Column(db.String(50))
+  alamat = db.Column(db.String(50))
+  gender = db.Column(db.String(10))
+  umur = db.Column(db.Integer())
+  photo = db.Column(db.String(100))
+  
+  
+
+  def __repr__(self):
+    return self.nama
+  
 
 
-def welcom_To_Itec():
-  return{
-         "message" : "Welcom to itec mataram"       
-  }
+
+@app.route("/list_pendaftaran")
+def semua_pendaftaran():
+    list_peserta = Peserta.query.all()
+    return render_template("list_pendaftaran.html",  tgl = "Tabel Peserta", lp=list_peserta)
+     
 
 
 
-if "__main__" ==__name__:
-     app.run(debug= True,port =2000)
